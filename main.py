07b5417b9d93a42b3c74ae25e03afd7b98d173a2
Verberne,Frans F.P.M <f.verberne@student.fontys.nl>
2022-03-02 16:50:04 +0100

    1e commit main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
 import mediapipe as mp
 
-mp_hands = mp.solutions.hands
+mp_hands = mp.solutions.hands  #scanner van de handen
 hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils # teken module
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) #langzaam openen opgelost door cap dshow
 
 
@@ -11,6 +12,14 @@ while True:
 
    results = hands.process(img)
    print(results.multi_hand_landmarks)
-   cv2.imshow("Live Feed", img)
-   cv2.waitKey(1)
 
+
+   if results.multi_hand_landmarks:
+      for hand_landmark in results.multi_hand_landmarks:
+         mp_draw.draw_landmarks(img, hand_landmark, mp_hands.HAND_CONNECTIONS,
+                                mp_draw.DrawingSpec((1, 190, 200), 2,2),
+                                mp_draw.DrawingSpec((18, 255, 255), 2,2)  # drawing specs via Tulp kleuren controle
+                                )
+
+   cv2.imshow("Hand Tracking", img)
+   cv2.waitKey(1)
