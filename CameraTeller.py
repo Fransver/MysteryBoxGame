23408c71c6a0742e39adac9d79489c937caf0070
Verberne,Frans F.P.M <f.verberne@student.fontys.nl>
2022-03-22 16:50:50 +0100

    Update handdetectors en counter

--- CameraTeller.py
+++ CameraTeller.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import mediapipe as mp
+import time # Timer maken voor aftellen vingertelling
 
 #================================
 # Handen creëren
@@ -13,52 +14,66 @@ hands = mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_co
 # Voor het actief tracken moet ik deze dus op FALSE zetten.
 # Min detection confidence op 0.5 is dat alle trackings die minder betrouwbaar als 50% zijn worden genegeerd.
 mp_drawing = mp.solutions.drawing_utils # tekenen om de handen
+
+#================================
+
+#Timer creëren
+timer = int(20)
+
 #================================
 
 
 
 def countFingers(image, results, draw=True, display=True):
 
+    # Breedte en Hoogte van de input
     height, width, _ = image.shape
-    # Create a copy of the input image to write the count of fingers on.
+
+    # Kopie maken om de vingers op te tekenen.
     output_image = image.copy()
 
-    # Initialize a dictionary to store the count of fingers of both hands.
+    # Een dictionary bevat niet te dupliceren items. Ze kunnen overschreven worden door een duplicate.
+    # Vorm lijkt een beetje op JSON.
     count = {'RIGHT': 0, 'LEFT': 0}
 
-    # Store the indexes of the tips landmarks of each finger of a hand in a list.
-    fingers_tips_ids = [mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
-                        mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.PINKY_TIP]
-
-    # Initialize a dictionary to store the status (i.e., True for open and False for close) of each finger of both hands.
+    # Wederom een dictionary voor de boolean waardes van beide handen.
     fingers_statuses = {'RIGHT_THUMB': False, 'RIGHT_INDEX': False, 'RIGHT_MIDDLE': False, 'RIGHT_RING': False,
                         'RIGHT_PINKY': False, 'LEFT_THUMB': False, 'LEFT_INDEX': False, 'LEFT_MIDDLE': False,
                         'LEFT_RING': False, 'LEFT_PINKY': False}
 
-# Iterate over the found hands in the image.
+    # De vingertoppen in een array stoppen.
+    fingers_tips = [mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.MIDDLE_FINGER_TIP,
+                        mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.PINKY_TIP]
+
+
+
+    # Iterate over de gevonden handen
     for hand_index, hand_info in enumerate(results.multi_handedness):
-        # Retrieve the label of the found hand.
+
+        # Een label voor links of rechts toewijzen aan de gevonden handen.
         hand_label = hand_info.classification[0].label
 
-        # Retrieve the landmarks of the found hand.
+        # De landmarks van de gevonden handen teruggeven.
         hand_landmarks = results.multi_hand_landmarks[hand_index]
 
         # Iterate over the indexes of the tips landmarks of each finger of the hand.
-        for tip_index in fingers_tips_ids:
+        for tip_index in fingers_tips:
             # Retrieve the label (i.e., index, middle, etc.) of the finger on which we are iterating upon.
             finger_name = tip_index.name.split("_")[0]
 
             # Dit is eigenlijk het belangrijkste van wat er gebeurt in het script
+            # Ik gebruik hier de vingertoppen en als die boven het middenpunt komen verandert de status
+            # Ook komt er dan een punt bij voor iedere opgestoken vinger
             if (hand_landmarks.landmark[tip_index].y <  hand_landmarks.landmark[tip_index - 2].y):
                 fingers_statuses[hand_label.upper() + "_" + finger_name] = True
+
                 # Increment the count of the fingers up of the hand by 1.
                 count[hand_label.upper()] += 1
 
             # Retrieve the y-coordinates of the tip and mcp landmarks of the thumb of the hand.
             thumb_tip_x = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x
             thumb_mcp_x = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP - 2].x
-            # Check if the thumb is up by comparing the hand label and the x-coordinates of the retrieved landmarks.
-            # Check if the thumb is up by comparing the hand label and the x-coordinates of the retrieved landmarks.
+
             if (hand_label == 'Right' and (thumb_tip_x < thumb_mcp_x)) or (hand_label == 'Left' and (thumb_tip_x < thumb_mcp_x)):
                     # Update the status of the thumb in the dictionary to true.
                     fingers_statuses[hand_label.upper() + "_THUMB"] = True
@@ -69,11 +84,17 @@ def countFingers(image, results, draw=True, display=True):
 
     if draw:
         # Hier schrijf ik het totaal aantal vingers in de camera-feed
-        cv2.putText(output_image, " Total Fingers: ", (25, 25), cv2.FONT_HERSHEY_COMPLEX, 1, (205,51,51), 2)
+        cv2.putText(output_image, " Code invoer: ", (25, 25), cv2.FONT_HERSHEY_COMPLEX, 1, (205,51,51), 2)
         cv2.putText(output_image, str(sum(count.values())), (width // 2 - 150, 240), cv2.FONT_HERSHEY_SIMPLEX,
                     3, (220,20,60), 10, 10 ) #grootte, kleur, dikte cijfers
 
-    # Check if the output image is specified to be displayed.
+        # Hier ergens een if om de code te corresponderen ?
+        if (sum(count.values()) == 2):
+            cv2.putText(output_image, "Leuk", (270, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        else:
+            cv2.putText(output_image, "Niet leuk", (270, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+
     if display:
         # Display the output image.
         plt.figure(figsize=[10, 10])
@@ -85,8 +106,8 @@ def countFingers(image, results, draw=True, display=True):
         # Return the output image, the status of each finger and the count of the fingers up of both hands.
         return output_image, fingers_statuses, count
 
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) #langzaam openen opgelost door cap dshow
-cv2.namedWindow('Fingers Counter', cv2.WINDOW_NORMAL)
 while cap.isOpened(): # connectie met camera
 
     # Read a frame.
@@ -108,7 +129,7 @@ while cap.isOpened(): # connectie met camera
         frame, fingers_statuses, count = countFingers(frame, results, display=False)
 
     # Display the frame.
-    cv2.imshow('Fingers Counter', frame)
+    cv2.imshow('VingerTellerCode', frame)
 
     # Wait for 1ms. If a key is pressed, retreive the ASCII code of the key.
     k = cv2.waitKey(1)
